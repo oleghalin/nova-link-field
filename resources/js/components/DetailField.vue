2fddf74dd3d0a3166e06fd001685a5375311738a
Keoghan Litchfield <keoghan@klever.co.uk>
2023-02-09 13:29:49 +0000

    Nova4 Upgrade

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex border-b border-40">
-        <div class="w-1/4 py-4">
+        <div class="md:w-1/4 md:py-3">
             <slot>
                 <h4 class="font-normal text-80">
                     {{ field.name }}
                 </h4>
             </slot>
         </div>
-        <div class="w-3/4 py-4">
+        <div class="md:w-3/4 md:py-3 break-all lg:break-words">
             <slot name="value">
                 <p v-if="field.value" class="text-90">
                     <a :target="field.blank ? field.blank : '_self'" :class="field.classes ? field.classes : defaultClasses"
